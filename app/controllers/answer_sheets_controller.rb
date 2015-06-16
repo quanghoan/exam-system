302e0c2953fdf14a3803fb3054ecf0866ce9e5a1
quang hoan <quanghoan1992@gmail.com>
2015-06-16 08:51:32 +0700

    add answer sheet

--- app/controllers/answer_sheets_controller.rb
+++ app/controllers/answer_sheets_controller.rb
@@ -1,27 +1,39 @@
-class AnswerSheetsController < ApplicationController
+class AnswerSheetsController < ApplicationController  
   
   def show
     @answer_sheet = AnswerSheet.find(params[:id])
-    # @session_exam = SessionExam.find(params[:session_exam_id])   
+    # @session_exam = SessionExam.find(params[:session_exam_id]) 
+
   end
 
   def new
     @answer_sheet = AnswerSheet.new  
     @session_exam = SessionExam.find(params[:session_exam_id]) 
-    @exam = @session_exam.exam
-    
+    # @user = User.find(params[:user_id])
+    @exam = @session_exam.exam   
   end
 
-  def create
+  def create   
     @answer_sheet = AnswerSheet.new
-    @answer_sheet.user_id = params[:user_id]
-    # @answer_sheet.user_session_exam_id = params[:user_session_exam_id]    
+    # @user = User.find(params[:user_id])
+    # @answer_sheet.user_id = params[:user_id]
+    # @answer_sheet.user_session_exam_id = params[:user_session_exam_id] 
+    @session_exam = SessionExam.find(params[:session_exam_id])
+    @answer =params[:answer]
+    # @question = params[:question]
+    # binding.pry
     if @answer_sheet.save
       flash[:success] = 'successfully submitted.'
-      redirect_to answer_sheet_path(@answer_sheet)
+      # redirect_to answer_sheet_path(@answer_sheet)
     else
       flash[:danger] = 'failed.'
-      redirect_to answer_sheet_path(@answer_sheet)
+      # redirect_to answer_sheet_path(@answer_sheet)
     end
   end
+
+  # private
+
+  # def answer_sheet_params
+  #   params.require(:answer_sheet).permit(:user_id, :session_exam_id, :user_session_exam_id )
+  # end
 end
--- app/models/answer_sheet.rb
+++ app/models/answer_sheet.rb
@@ -2,7 +2,7 @@ class AnswerSheet < ActiveRecord::Base
 	has_many :answers, dependent: :destroy
 	belongs_to :user_session_exam
 	belongs_to :user 
-	belongs_to :time_session
+	
 	accepts_nested_attributes_for :answers
 	has_one :session_exam
 end
